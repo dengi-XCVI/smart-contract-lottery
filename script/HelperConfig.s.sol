a0839cc87cefa41427c9b8b4269a9f9b9c8f6ca9
dengidengi <dengianassi@gmail.com>
2025-09-21 17:07:35 +0200

    Fund subscription script added

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -5,6 +5,7 @@ pragma solidity 0.8.19;
 import {Script} from 'forge-std/Script.sol';
 import {Raffle} from '../src/Raffle.sol';
 import {VRFCoordinatorV2_5Mock} from '@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol';
+import {LinkToken} from '../test/mocks/LinkToken.sol';
 
 abstract contract CodeConstants {
     uint256 public constant ETH_SEPOLIA_CHAINID = 11155111;
@@ -27,6 +28,7 @@ contract HelperConfig is Script,CodeConstants {
         uint256 subscriptionId;
         uint32 callbackGasLimit;
         address vrfCoordinator;
+        address link;
     }
 
     NetworkConfig public localNetworkConfig;
@@ -59,7 +61,8 @@ contract HelperConfig is Script,CodeConstants {
             vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
             gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
             subscriptionId: 0,
-            callbackGasLimit:500000 // 500,000 gas
+            callbackGasLimit:500000, // 500,000 gas
+            link: 0x779877A7B0D9E8603169DdbD7836e478b4624789
         });
     }
 
@@ -74,6 +77,7 @@ contract HelperConfig is Script,CodeConstants {
             MOCK_GAS_PRICE_LINK,
             MOCK_WEI_PER_UINT_LINK
         );
+        LinkToken linkToken = new LinkToken();
         vm.stopBroadcast();
         localNetworkConfig = NetworkConfig({
             entranceFee: 0.01 ether,
@@ -82,7 +86,8 @@ contract HelperConfig is Script,CodeConstants {
             // doesn't matter, mock makes it work
             gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
             subscriptionId: 0,
-            callbackGasLimit:500000 // 500,000 gas
+            callbackGasLimit:500000, // 500,000 gas
+            link: address(linkToken)
         });
         return localNetworkConfig;
 
--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -3,8 +3,9 @@
 pragma solidity 0.8.19;
 
 import {Script, console} from 'forge-std/Script.sol';
-import {HelperConfig} from './HelperConfig.s.sol';
+import {HelperConfig, CodeConstants} from './HelperConfig.s.sol';
 import {VRFCoordinatorV2_5Mock} from '@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol';
+import {LinkToken} from '../test/mocks/LinkToken.sol';
 
 contract CreateSubscription is Script {
 
@@ -29,4 +30,34 @@ contract CreateSubscription is Script {
     function run() external{
         createSubscriptionUsingConfig();
     }
+}
+
+contract FundSubscription is Script, CodeConstants {
+
+    uint256 public constant FUND_AMOUNT = 3 ether; // 3 LINK
+
+    function fundSubscriptionUsingConfig() public {
+        HelperConfig helperConfig = new HelperConfig();
+        address vrfCoordinator = helperConfig.getConfig().vrfCoordinator;
+        uint256 subscritpionId = helperConfig.getConfig().subscriptionId;
+        address linkToken = helperConfig.getConfig().link;
+        fundSubscription(vrfCoordinator, subscritpionId, linkToken);
+    }
+
+    function fundSubscription(address vrfCoordinator, uint256 subscriptionId, address linkToken) public {
+        console.log("Funding subscription:", subscriptionId);
+        console.log("VRFCoordinator:",vrfCoordinator);
+        console.log("On chain id:",block.chainid);
+        if (block.chainid == ANVIL_CHAINID) {
+            vm.startBroadcast();
+            VRFCoordinatorV2_5Mock(vrfCoordinator).fundSubscription(subscriptionId,FUND_AMOUNT);
+            vm.stopBroadcast();
+        } else {
+            vm.startBroadcast();
+            LinkToken(linkToken).transferAndCall(vrfCoordinator, FUND_AMOUNT,abi.encode(subscriptionId));
+            vm.stopBroadcast();
+        }
+    }
+
+    function run() external {}
 }
